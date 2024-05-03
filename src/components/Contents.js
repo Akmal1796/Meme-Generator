025af28bfad71eefe071ae53f54496ea0f20cc04
Akmal1796 <mhdakmal1796@gmail.com>
2024-05-03 22:32:05 +0530

    Completed the from on the contents section

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,7 +1,20 @@
 import React from "react";
+import memesData from "../memesData.js"
 
 export default function Contents() {
+
+    function getMemeImage() {
+        console.log("Hover!");
+    }
+
     return(
-        <h1>Contents</h1>
+        <main className="main">
+            <form className="form">
+                <input type="text" className="input-text" placeholder="Top Text"/>
+                <input type="text" className="input-text" placeholder="Bottom Text"/>
+                <button onClick={getMemeImage} className="btn">Get new meme image 🖼️</button>
+            </form>
+            <img src="../images/img.jpg" className="meme-img"/>
+        </main>
     )
 }
