ec1667a9b2089de5849a15b66a62a14f20f16370
Akmal1796 <mhdakmal1796@gmail.com>
2024-05-09 12:20:04 +0530

    finally completed the meme generator basic functionality

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,30 +1,16 @@
 import React from "react";
-import memesData from "../memesData.js"
+import memesData from '../memesData';
 
-export default function Contents() {
+export default function Contents() {    
+
+    const [memeImage, setMemeImage] = React.useState('');
 
     function getMemeImage() {
         const memeArray = memesData.data.memes;
         const randumNUM = Math.floor(Math.random() * memeArray.length);const url = memeArray[randumNUM].url;/* const {url} = memeArray[randumNUM] */
-        console.log(url);
+        //console.log(url);
+        setMemeImage(memeArray[randumNUM].url)
     }
-
-    const things = ["Thing 1", "Thing 2"];
-
-    const thingsElements = things.map(thing => <p key={thing}>{thing}</p>);
-
-    function clickHandle() {
-        const thingText = `Thing ${things.length + 1}`;
-        things.push(thingText);
-        console.log(things);
-    }
-
-    /* React State */
-
-/*     const result = React.useState("Yes"); */
-
-        /* React State Destructuring */
-    const [result, func] = React.useState("Yes");
 
     return(
         <main className="main">
@@ -33,12 +19,7 @@ export default function Contents() {
                 <input type="text" className="input-text" placeholder="Bottom Text"/>
                 <button onClick={getMemeImage} className="btn">Get new meme image 🖼️</button>
             </div>
-            <img src="../images/img.jpg" className="meme-img"/>
-            <div>
-                <button onClick={clickHandle}>Add Item</button>
-                {thingsElements}
-                <p>{result}</p>
-            </div>
+            <img src={memeImage} className="meme-img"/>
         </main>
     )
 }
