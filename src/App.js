3c38cb78933f022fe25cf3acef5d5bfdecf95eff
Akmal1796 <mhdakmal1796@gmail.com>
2024-05-20 18:20:32 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Contents from './components/Contents';
+import Counter from './components/Counter';
 
 function App() {
+/* 
+  const [count, setCount] = React.useState(0);
+
+  function add() {
+      setCount(prevCount => prevCount +  1);
+  }
+
+  function substract() {
+      setCount(prevCount => prevCount - 1);
+  }
+ */
   return (
     <div className="App">
         <Header />
         <Contents />
+{/*       <div>
+          <button  onClick={substract}>-</button>
+          <Counter number={count} />
+          <button onClick={add}>+</button>
+      </div> */}
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Counter(props){
+
+  return (
+    <div>
+        <h1>{props.number}</h1>
+    </div>
+  )
+}
+
+export default Counter
